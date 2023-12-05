51a7565c49cc72b1b6091cadc822f5f4be03dc85
satrioed <32364936+satrio13@users.noreply.github.com>
2023-12-05 15:42:56 +0700

    Update form_edit_kelas.php

--- application/modules/kelas/views/form_edit_kelas.php
+++ application/modules/kelas/views/form_edit_kelas.php
@@ -1,15 +1,7 @@
 <?php
-if(isset($_POST['submit']))
-{
-  $kelas = $this->input->post('kelas', TRUE);
-  $tingkat = $this->input->post('tingkat', TRUE);
-  $id_jurusan = $this->input->post('id_jurusan', TRUE);
-}else
-{
-  $kelas = $data->kelas;
-  $tingkat = $data->tingkat;
-  $id_jurusan = $data->id_jurusan;
-}
+$id_tahunpelajaran = isset($_POST['submit']) ? $this->input->post('id_tahunpelajaran', TRUE) : $data->id_tahunpelajaran;
+$id_kelas = isset($_POST['submit']) ? $this->input->post('id_kelas', TRUE) : $data->id_kelas;
+$id_guru = isset($_POST['submit']) ? $this->input->post('id_guru', TRUE) : $data->id_guru;
 ?>
 <div class="content-wrapper">
   <div class="content-header">
@@ -21,7 +13,7 @@ if(isset($_POST['submit']))
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="<?= base_url('backend'); ?>">Home</a></li>
-            <li class="breadcrumb-item"><a href="<?= base_url('backend/kelas'); ?>">Kelas</a></li>
+            <li class="breadcrumb-item"><a href="<?= base_url('backend/kelas-wali'); ?>">Wali Kelas</a></li>
             <li class="breadcrumb-item active"><?= $title; ?></li>
           </ol>
         </div>
@@ -33,47 +25,51 @@ if(isset($_POST['submit']))
     <div class="row">
         <div class="col-12">
             <?php 
-            if($this->session->flashdata('msg-kelas'))
+            if($this->session->flashdata('msg-kelas-wali'))
             {
-                echo pesan_sukses($this->session->flashdata('msg-kelas'));
-            }elseif($this->session->flashdata('msg-gagal-kelas'))
+              echo pesan_sukses($this->session->flashdata('msg-kelas-wali'));
+            }elseif($this->session->flashdata('msg-gagal-kelas-wali'))
             {
-                echo pesan_gagal($this->session->flashdata('msg-gagal-kelas'));
+              echo pesan_gagal($this->session->flashdata('msg-gagal-kelas-wali'));
             }
             ?>
             <div class="card card-info">
                 <div class="card-header">
                     <h3 class="card-title">FORM <?= strtoupper($title); ?></h3>
                 </div>
-                <?php echo form_open('backend/edit-kelas/'.$this->uri->segment('3'), 'id="form"'); ?>
+                <?php echo form_open('backend/edit-kelas-wali/'.$this->uri->segment('3'), 'id="form"'); ?>
                 <div class="card-body">
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">TAHUN PELAJARAN <span class="text-danger">*</span></label>
+                        <div class="col-sm-5">
+                            <select name="id_tahunpelajaran" class="form-control required">
+                                <?php foreach($tahunpelajaran as $r): ?>
+                                    <option value="<?= $r->id_tahunpelajaran; ?>" <?php if($id_tahunpelajaran == $r->id_tahunpelajaran){ echo'selected'; } ?> ><?= $r->tahunpelajaran; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                            <?php echo form_error('id_tahunpelajaran'); ?>
+                        </div>
+                    </div>
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label">KELAS <span class="text-danger">*</span></label>
                         <div class="col-sm-5">
-                            <input type="text" name="kelas" value="<?= $kelas; ?>" class="form-control required" placeholder="KELAS">
-                            <?php echo form_error('kelas'); ?>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">TINGKAT <span class="text-danger">*</span></label>
-                        <div class="col-sm-5">
-                            <select name="tingkat" class="form-control required">
-                                <option value="X" <?php if($tingkat == 'X'){ echo'selected'; } ?> >X</option>
-                                <option value="XI" <?php if($tingkat == 'XI'){ echo'selected'; } ?> >XI</option>
-                                <option value="XII" <?php if($tingkat == 'XII'){ echo'selected'; } ?> >XII</option>
-                            </select>
-                            <?php echo form_error('tingkat'); ?>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">JURUSAN <span class="text-danger">*</span></label>
-                        <div class="col-sm-5">
-                            <select name="id_jurusan" class="form-control required">
-                                <?php foreach($jurusan as $r): ?>
-                                    <option value="<?= $r->id_jurusan; ?>" <?php if($id_jurusan == $r->id_jurusan){ echo'selected'; } ?> ><?= $r->jurusan; ?></option>
+                            <select name="id_kelas" class="form-control required">
+                                <?php foreach($kelas as $r): ?>
+                                    <option value="<?= $r->id_kelas; ?>" <?php if($id_kelas == $r->id_kelas){ echo'selected'; } ?> ><?= $r->kelas; ?></option>
                                 <?php endforeach; ?>
                             </select>
-                            <?php echo form_error('id_jurusan'); ?>
+                            <?php echo form_error('id_kelas'); ?>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">GURU <span class="text-danger">*</span></label>
+                        <div class="col-sm-5">
+                            <select name="id_guru" class="form-control required">
+                                <?php foreach($guru as $r): ?>
+                                    <option value="<?= $r->id_guru; ?>" <?php if($id_guru == $r->id_guru){ echo'selected'; } ?> ><?= $r->nama; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                            <?php echo form_error('id_guru'); ?>
                         </div>
                     </div>
                     <div class="form-group row">
@@ -84,7 +80,7 @@ if(isset($_POST['submit']))
                 </div>
                 <div class="card-footer">
                   <button type="submit" name="submit" value="Submit" class="btn btn-info btn-flat"><i class="fa fa-check"></i> SIMPAN</button>
-                  <a href="<?= base_url(); ?>backend/kelas" class="btn btn-danger btn-flat float-right"><i class="fa fa-arrow-left"></i> BATAL</a>
+                  <a href="<?= base_url(); ?>backend/kelas-wali" class="btn btn-danger btn-flat float-right"><i class="fa fa-arrow-left"></i> BATAL</a>
                 </div>
               <?php echo form_close() ?>
             </div>
